6f09213b3781e4a7ede6af23f1793178da5300f9
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-02 15:08:46 +0530

    update2commit

--- Activity_04.py
+++ Activity_04.py
@@ -1,3 +1,7 @@
 a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
-print("%d + %d = %d"%(a,b,a+b))
+c = a+b
+print("%d+%d=%d"%(a,b,c))
+print("{n1}+{n2}={n3}".format(n1=a,n2=b,n3=c))
+print("{0}+{1}={2}".format(a,b,c))
+print("{}+{}={}".format(a,b,c)
