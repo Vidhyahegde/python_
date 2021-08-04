5ed1cc40f7691b8b49949cd499eb4900417f2bee
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-04 10:14:54 +0530

    update1.commit

--- Activity_15.py
+++ Activity_15.py
@@ -1,7 +1,8 @@
-y=sorted(input("Enter a list of strings").split())
+def inp():
+    return input("Enter a list of strings")
+y=sorted(inp().split())
 print(y)
-z=input("Enter a number of strings").split()
+z=inp().split()
 a=list(z)
 a.sort()
 print(a)
-
