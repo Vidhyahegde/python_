ece0e081aa8284cce56b3c52b3989883bc87f28d
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-04 12:05:20 +0530

    donee.commit

--- Activity_06.py
+++ Activity_06.py
@@ -1,14 +1,12 @@
-x = input("Enter 5 numbers")
+x = input("Enter 5 numbers ")
 y=x.split()
-list1=list(y)
-print(list1)
-l2=list1[::-1]
-print("reversed list =",l2)
-l1=list1[0:3]
-print("sliced list=",l1)
-list1[-1]=0
-list1[0]=0
-print("replaced list-1 =",list1)
+print(y)
+l2=y[::-1]
+print("reversed list = ",l2)
+l1=y[0:3]
+print("sliced list= ",l1)
+y[0]=0
+print("replaced list-1 = ",y)
 l1[0]=0
 l1[-1]=0
-print("replaced list-2 =",l1)
+print("replaced list-2 = ",l1)
