38d67a207183b7569ecc6c96280f0da855f0c623
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-04 10:18:41 +0530

    update1.commit

--- Activity_11.py
+++ Activity_11.py
@@ -1,18 +1,17 @@
 def main():
-    a = input_number()
+    a = intput_number()
     b = input_number()
     summation = add(a, b)
     display(a, b, summation)
 
 def input_number():
     return int(input("Enter an integer"))
-    
+def intput_number():
+    return int(input("Enter an integer"))
 def display(x,y,z):
     print(x,y,z)
-     
 def add(x,y):
     s=x+y
     return s
      
 main()
-
