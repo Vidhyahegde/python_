1df32a7bf6d167d28ffd94b214cdf29714308b1c
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-04 16:29:07 +0530

    update2.commit

--- Activity_06.py
+++ Activity_06.py
@@ -1,7 +1,5 @@
-x = input('enter the elements')
-y = x.split()
-for i in range(0, len(y)):
-    y[i] = int(y[i])
+x=map(int,input("Enter five integers ").split())
+y=list(x)
 z=y[::-1]
 print("reversed list = ",z)
 l1=y[0:3]
@@ -9,9 +7,6 @@ print("sliced list = ",l1)
 y[0]=0
 y[-1]=0
 print("replace list1 = ",y)
-l1[0]=0
 l1[-1]=0
+l1[0]=0
 print("replace list2 = ",l1)
-
-
-
