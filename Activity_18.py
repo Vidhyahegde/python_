fc4a9e8a8ee9c0bf6dff1a23da8ca594b670ba98
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-05 14:20:42 +0530

    update.commit

---
+++ Activity_18.py
@@ -0,0 +1,13 @@
+def inp():
+    a=input("Enter string here - ")
+    return a
+def tup(a):
+    b=list()
+    for i in a.split(';'):
+        c=i.split('=')
+        b.append(tuple(c))
+    return b
+y=inp()
+x=tup(y)
+print(dict(x))
+
