11b0bd6496d5403f623276e43a0ced14282d517e
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-19 10:14:32 +0530

    update.commit

--- Activity_19.py
+++ Activity_19.py
@@ -1,16 +1,18 @@
 def main():
     y={}
-    for i in range(number()):
-        key=input("Enter key:")
+    n=number()
+    for i in range(n):
+        key=input("Enter key :")
         value = input("Enter value :")
         y[key]=value
-    print(y)
+    print("Before sorting - ",y)
     from collections import OrderedDict
     dict1 = OrderedDict(sorted(y.items()))
-    print(dict(dict1))
+    print("After sorting - ",dict(dict1))
 
-        
+
 def number():
-    n=int(input("Numberof elements in the dictionary"))
+    n=int(input("Enter the number of elements in the dictionary - "))
     return n
+
 main()
