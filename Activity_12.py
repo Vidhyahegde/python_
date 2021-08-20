163ad3abb71b61bb8d11c0fff8ad80edf2a9279a
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-20 13:24:13 +0530

    update.commit

--- Activity_12.py
+++ Activity_12.py
@@ -1,12 +1,20 @@
 def inp():
-    return input("Enter three numbers")
-
-z=inp().split()
-
-
-p=int(z[0])
-q=int(z[1])
-r=int(z[2])
-
-a=max(p,q,r)
-print(a,"is the greatest number among ",p,",",q," and" ,r)
+    return input("Enter three numbers - ")
+     
+def main():
+    x=inp()
+    y=x.split()
+    z=list(y)
+    p=int(z[0])
+    q=int(z[1])
+    r=int(z[2])
+    m=ma(p,q,r)
+    print(m,"is the greatest number among ",p,",",q," and" ,r)
+def ma(p,q,r):
+    if p>q and p>r:
+        return p
+    elif q>p and q>r:
+        return q
+    else:
+        return r
+main()
