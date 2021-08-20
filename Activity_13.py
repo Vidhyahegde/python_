943967da7f14ff2148b0b28f9f4ef51b00ae34a1
Vidhyahegde <88309662+Vidhyahegde@users.noreply.github.com>
2021-08-20 14:28:51 +0530

    update.commit

--- Activity_13.py
+++ Activity_13.py
@@ -1,16 +1,26 @@
 def prime_no(n):
-    for i in (1,n):
-        if (n%i!=0):
-            print(n,"is a prime number")
-            break
-        elif(n==2):
-            print(n,"is the only even prime number")
-            break
-        elif(n==1):
-            print(n,"is neither prime nor compposite")
-            break
-        else:
+    if n==1:
+        print("Neither prime nor composite")
+        return
+    if n==2:
+        print(n," is the only even prime number")
+        return
+    
+    
+    for i in range(2,int(n**.5)+1):
+        if (n%i==0):
             print(n,"is not a prime number")
             break
-x=int(input("Enter a number"))
-prime_no(x)
+    else:
+        print(n," is a prime number")
+    
+            
+def main():
+    x = number()
+    prime_no(x)
+
+def number():
+    return int(input("Enter a number - "))
+
+main()
+
